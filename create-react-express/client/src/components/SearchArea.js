2d85c17dff76bf17cbeae4a39b773f9bc316163f
WideSpreadX <48499429+WideSpreadX@users.noreply.github.com>
2019-09-05 14:25:08 -0400

    added git

---
+++ create-react-express/client/src/components/SearchArea.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import SearchBar from "./components/SearchBar";
+import SearchBar from "./components/SearchBar";
+import SearchButton from "./components/SearchButton";
+import "./App.css";
+
+class SearchArea extends Component {
+    render() {
+        return (
+            <SearchBar />
+            <SearchButton />
+    );
+    }
+}
+
+export default SearchArea;
